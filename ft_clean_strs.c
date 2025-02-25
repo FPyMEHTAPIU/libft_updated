e3e98b1d0be4f2718f8cbe0cc5822b5e77dff247
msavelie <msavelie@student.hive.fi>
2025-02-25 17:29:00 +0200

    Fix ft_clean_strs.

--- ft_clean_strs.c
+++ ft_clean_strs.c
@@ -6,7 +6,7 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:20:49 by msavelie          #+#    #+#             */
-/*   Updated: 2024/12/05 16:22:39 by msavelie         ###   ########.fr       */
+/*   Updated: 2025/02/25 17:28:26 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void	ft_clean_strs(char **strs)
 {
 	int	i;
 
-	if (!strs || !*strs)
+	if (!strs)
 		return ;
+	if (!*strs)
+	{
+		free(strs);
+		return ;
+	}
 	i = 0;
 	while (strs[i])
 	{
